6c45146fc62236d7c83c7b9b8ffbd61c7d535b5d
Adrian Friedli <masteradi@gmx.ch>
2007-05-19 23:44:32 +0200

    epoch, unsupported_op and unsupported_version support added

--- natpmp.c
+++ natpmp.c
@@ -6,11 +6,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <time.h>
 
 #include "natpmp_defs.h"
 
 /* file descriptor for the udp socket */
 int sfd;
+
+/* time this daemon has been started or tables got refreshed */
+time_t timestamp;
 
 /* function for clean dying */
 void p_die(const char * p) {
@@ -19,7 +23,13 @@ void p_die(const char * p) {
 	exit(EXIT_FAILURE);
 }
 
-/* function for sending */
+/* function for sending, if t_addr is given */
+void udp_send_r(struct sockaddr_in * t_addr, void * data, size_t len) {
+	int err = sendto(sfd, data, len, MSG_DONTROUTE, (struct sockaddr *) t_addr, sizeof(*t_addr));
+	if (err == -1) p_die("sendto");
+}
+
+/* function for sending, if only destination address and port are given */
 void udp_send(in_addr_t address, in_port_t port, void * data, size_t len) {
 	/* construct target socket address */
 	struct sockaddr_in t_addr;
@@ -29,11 +39,29 @@ void udp_send(in_addr_t address, in_port_t port, void * data, size_t len) {
 	t_addr.sin_addr.s_addr = htonl(address);
 
 	/* send it */
-	int err = sendto(sfd, data, len, MSG_DONTROUTE, (struct sockaddr *) &t_addr, sizeof(t_addr));
-	if (err == -1) p_die("sendto");
+	udp_send_r(&t_addr, data, len);
+}
+
+/* return seconds since daemon started */
+uint32_t get_epoch() {
+	return time(NULL) - timestamp;
+};
+
+/* being called on unsupported functions */
+void unsupported(uint16_t result, natpmp_packet_dummy_request * packet, struct sockaddr_in * t_addr) {
+	natpmp_packet_dummy_answer answer_packet;
+	answer_packet.header.version = NATPMP_VERSION;
+	/* it's not defined which op should be sent on undefined versions, so this should be ok, too */
+	answer_packet.header.op = packet->header.op | NATPMP_ANSFLAG;
+	answer_packet.answer.result = result;
+	answer_packet.answer.epoch = get_epoch();
+	udp_send_r(t_addr, &answer_packet, sizeof(answer_packet));
 }
 
 int main() {
+	/* set timestamp TODO: move to where tables get (re)loaded */
+	timestamp = time(NULL);
+
 	/* create UDP socket */
 	sfd = socket(PF_INET, SOCK_DGRAM, 0);
 	if (sfd == -1) p_die("socket");
@@ -42,7 +70,7 @@ int main() {
 	struct sockaddr_in s_addr;
 	memset(&s_addr, 0, sizeof(s_addr));
 	s_addr.sin_family = AF_INET;
-	s_addr.sin_port = htons(2039);
+	s_addr.sin_port = htons(NATPMP_PORT);
 	s_addr.sin_addr.s_addr = 0;
 
 	/* bind here */
@@ -69,17 +97,26 @@ int main() {
 		if (err == -1) p_die("recvfrom");
 		else if (err == 0) continue;
 
-		/* do things depending on the op code */
-		if (packet.header.op & NATPMP_ANSFLAG) continue;
-		switch (packet.header.op & (~NATPMP_ANSFLAG)) {
+		/* do things depending on the packet content */
+		if (err < sizeof(natpmp_packet_dummy_request)) continue; /* TODO errorlog */
+		if (packet.header.version != NATPMP_VERSION)
+			unsupported(NATPMP_UNSUPPORTEDVERSION, (natpmp_packet_dummy_request *) &packet, &t_addr);
+		if (packet.header.op & NATPMP_ANSFLAG) continue; /* TODO errorlog */
+		switch (packet.header.op) {
 			case NATPMP_PUBLICIPADDRESS :
+				if (err != sizeof(natpmp_packet_publicipaddress_request)) continue; /* TODO errorlog */
+				/* TODO */
 				break;
 			case NATPMP_MAP_UDP :
+				if (err != sizeof(natpmp_packet_map_request)) continue; /* TODO errorlog */
+				/* TODO */
 				break;
 			case NATPMP_MAP_TCP :
+				if (err != sizeof(natpmp_packet_map_request)) continue; /* TODO errorlog */
+				/* TODO */
 				break;
 			default :
-				break;
+				unsupported(NATPMP_UNSUPPORTEDOP, (natpmp_packet_dummy_request *) &packet, &t_addr);
 		}
 	}
 
--- natpmp_defs.h
+++ natpmp_defs.h
@@ -1,3 +1,6 @@
+#define NATPMP_PORT 5351
+#define NATPMP_MAX_PAYLOADSIZE 112
+
 #define NATPMP_VERSION 0
 #define NATPMP_ANSFLAG 0x80
 #define NATPMP_PUBLICIPADDRESS 0
@@ -9,8 +12,6 @@
 #define NATPMP_NETFAILURE 3
 #define NATPMP_OUTOFRESOURCES 4
 #define NATPMP_UNSUPPORTEDOP 5
-
-#define NATPMP_MAX_PAYLOADSIZE 112
 
 
 typedef struct {
@@ -37,7 +38,12 @@ typedef struct {
 
 typedef struct {
 	_natpmp_header header;
-} natpmp_packet_publicipaddress_request;
+} natpmp_packet_dummy_request, natpmp_packet_publicipaddress_request;
+
+typedef struct {
+	_natpmp_header header;
+	_natpmp_answer answer;
+} natpmp_packet_dummy_answer;
 
 typedef struct {
 	_natpmp_header header;
