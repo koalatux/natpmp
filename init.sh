6f063468afb97ba180b7721263cc930796d78e8f
Adrian Friedli <masteradi@gmx.ch>
2007-06-21 20:54:52 +0200

    Init script reworked, now POSIX compatible.

--- init.sh
+++ init.sh
@@ -21,13 +21,36 @@
 
 PUBLIC_IF=eth0
 PRIVATE_IFS=eth2
+IPTABLES_CHAIN=natpmp
 
-BIND_ADDRS=""
+# Flush all the rules in the natpmp chain, or create it, if it doesn't exists.
+/sbin/iptables -t nat -F $IPTABLES_CHAIN 2>/dev/null || \
+/sbin/iptables -t nat -N $IPTABLES_CHAIN
+
+# Handle all incoming connections in the natpmp chain.
+/sbin/iptables -t nat -D PREROUTING -j $IPTABLES_CHAIN 2>/dev/null || true
+/sbin/iptables -t nat -A PREROUTING -j $IPTABLES_CHAIN
+#-i $PUBLIC_IF
+
+# Iterate through the private interfaces.
+BIND_ARGS=""
 for IF in $PRIVATE_IFS; do
-	ip route | grep "^224\.0\.0\.0/4 dev $IF" > /dev/null || ip route add 224.0.0.0/4 dev $IF
-	ADDR=`ip addr show dev $IF | grep "^\s*inet .* $IF\$"| cut -d " " -f 6 | cut -d / -f 1`
-	[ -n "$ADDR" ] && BIND_ADDRS+=" --bind $ADDR"
+	# Get the IP address of this interface.
+	ADDR=`/sbin/ip addr show dev $IF 2>/dev/null | grep "^\s*inet .* $IF\$" | cut -d " " -f 6 | cut -d / -f 1`
+	if [ -n "$ADDR" ] ; then
+		# Add the IP address to the argument list.
+		BIND_ARGS="$BIND_ARGS --bind \"$ADDR\""
+		# Add the multicast route for this interface if it doesn't exist already.
+		/sbin/ip route | grep "^224\.0\.0\.0/4 dev $IF" > /dev/null || /sbin/ip route add 224.0.0.0/4 dev $IF
+	else
+		echo "Could not get IP address of interface $IF. Skipping." >&2
+	fi
 done
 
-./natpmp
-#natpmp --public-if $PUBLIC_IF $BIND_ADDRS
+if [ -z "$BIND_ARGS" ] ; then
+	echo "No IP addresses to bind to. Exiting." >&2
+	exit 1
+fi
+
+exec ./natpmp
+#exec /usr/sbin/natpmp --pub-if "$PUBLIC_IF" $BIND_ARGS -- --chain "$IPTABLES_CHAIN"
