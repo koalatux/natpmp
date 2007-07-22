47ac64fbb0acd3628f5572d1051ea89e2199e441
Adrian Friedli <masteradi@gmx.ch>
2007-07-22 02:42:24 +0200

    added links to savvannah project page, version set to 0.1.0

--- natpmp.c
+++ natpmp.c
@@ -17,7 +17,7 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#define PROGRAM_VERSION "0.1.0alpha3"
+#define PROGRAM_VERSION "0.1.0"
 
 #include <arpa/inet.h>
 //#include <sys/types.h>
