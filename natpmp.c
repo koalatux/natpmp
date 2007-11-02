e034ef9b82f6117220874071e295ac04e4d84a4c
Adrian Friedli <masteradi@gmx.ch>
2007-11-03 00:00:59 +0100

    releasing version 0.2.0

--- natpmp.c
+++ natpmp.c
@@ -17,7 +17,7 @@
  *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#define PROGRAM_VERSION "0.1.1-devel"
+#define PROGRAM_VERSION "0.2.0"
 
 #include <arpa/inet.h>
 //#include <sys/types.h>
